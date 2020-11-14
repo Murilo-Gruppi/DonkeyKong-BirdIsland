aeb05cb1f2db3d0cd8510be278da4f14b6768625
Nexy8 <pedronexy88@gmail.com>
2020-11-14 15:54:12 -0300

    Imagens adicionadas

--- game.py
+++ game.py
@@ -3,8 +3,20 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 350))
 pygame.display.set_caption("Donkey Kong: Bird Island")
+background = pygame.image.load("images/background.png")
+ground = pygame.image.load("images/ground.fw.png")
+donkey_kong = pygame.image.load("images/donkey-kong.fw.png")
 
-while True:
+running = True
+
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            running = False
+
+    screen.blit(background, (0, 0))
+    screen.blit(ground, (0, 315))
+    screen.blit(donkey_kong, (20, 255))
+    pygame.display.update()
+
+pygame.quit()
