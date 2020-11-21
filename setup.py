2bc8205acdb89ee30b0751274ec23ce460d76fc4
Murilo <murilo.castro@ccc.ufcg.edu.br>
2020-11-21 17:10:54 -0300

    atualiza versão

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fread:
 
 setup(
     name="dkbirdisland",
-    version="0.1.0.dev9",
+    version="0.1.0",
     author="Murilo Castro",
     author_email="murilo.castro@ccc.ufcg.edu.br",
     description="DK, T-rex style",
